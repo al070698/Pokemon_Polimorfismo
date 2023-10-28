0a874b745c618c285ce6bf28a2a49cdd2be422e5
al070698 <145055756+al070698@users.noreply.github.com>
2023-10-27 20:58:32 -0600

    Update BatallaPokemon.java

--- BatallaPokemon.java
+++ BatallaPokemon.java
@@ -1,14 +1,12 @@
-import pokemon.Pokemon;
-import pokemon.Pikachu;
-import pokemon.Bulbasaur;
+package pokemon;
 
 public class BatallaPokemon {
-    public static void main(String args[]){
+    public static void main(String[] args) {
         Pokemon pikachu = new Pikachu("Pikachu", 1);
         Pokemon bulbasaur = new Bulbasaur("Bulbasaur", 1);
 
-        pidgey.atacar(0, bulbasaur, pikachu);
+        pikachu.atacar(0, bulbasaur, pikachu);
         System.out.println("\n");
-        charmander.atacar(2, pikachu, bulbasaur);
+        bulbasaur.atacar(2, pikachu, bulbasaur);
     }
 }
