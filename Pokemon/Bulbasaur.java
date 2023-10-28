8e27dcc6d8831388853d2503e1c07c54dd01c3cd
al070698 <145055756+al070698@users.noreply.github.com>
2023-10-27 20:54:40 -0600

    Update Bulbasaur.java

--- Pokemon/Bulbasaur.java
+++ Pokemon/Bulbasaur.java
@@ -1,15 +1,16 @@
 package pokemon;
 
-public class Bulbasaur extends Pokemon{
-    private Tipo tipo1 = Tipo.Planta, tipo2 = Tipo.VENENO ;
+public class Bulbasaur extends Pokemon {
+    private Tipo tipo1 = Tipo.PLANTA;
+    private Tipo tipo2 = Tipo.VENENO;
 
     public Bulbasaur(String nombre, int nivel){
         super(nombre, nivel, 39, 52, 43, 60, 50, 65);
         ListaMovimientos listaMovimientos = new ListaMovimientos();
 
-        setMovimientos(0, listaMovimientos.buscarMovimientoPorNombre("L├ítigo Cepa"));
+        setMovimientos(0, listaMovimientos.buscarMovimientoPorNombre("Látigo Cepa"));
         setMovimientos(1, listaMovimientos.buscarMovimientoPorNombre("Hoja Afilada"));
-	setMovimientos(2, listaMovimientos.buscarMovimientoPorNombre("Bomba Germen"));
+        setMovimientos(2, listaMovimientos.buscarMovimientoPorNombre("Bomba Germen"));
         setMovimientos(3, listaMovimientos.buscarMovimientoPorNombre("Bomba Lodo"));
     }
     
@@ -25,6 +26,20 @@ public class Bulbasaur extends Pokemon{
         if(pokemonAtacante.getMovimientos(m).getTipo()== Tipo.TIERRA) efectividad = 2.0;
 
         return efectividad;
+    }
+
+    @Override
+    public double calcularBonificacion(Pokemon pokemon, int pos) {
+        double bonificacion = 1.0;
+        Movimiento movimiento = this.getMovimientos(pos);
+        Tipo tipoMovimiento = movimiento.getTipo();
+        if (this.getTipo1() == tipoMovimiento || this.getTipo2() == tipoMovimiento) {
+            bonificacion = 1.5;
+        } else {
+            bonificacion = 1.0;
+        }
+
+        return bonificacion;
     }
 
     public Tipo getTipo1() {
@@ -43,3 +58,4 @@ public class Bulbasaur extends Pokemon{
         this.tipo2 = tipo2;
     }
 }
+
