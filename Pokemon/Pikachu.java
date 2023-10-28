d9ad93d85d7891887b45eb34a77a2e5850fd667f
al070698 <145055756+al070698@users.noreply.github.com>
2023-10-27 20:41:13 -0600

    Update Pikachu.java

--- Pokemon/Pikachu.java
+++ Pokemon/Pikachu.java
@@ -7,13 +7,13 @@ public class Pikachu extends Pokemon {
         super(nombre, nivel, 40, 45, 40, 35, 35, 56);
         ListaMovimientos listaMovimientos = new ListaMovimientos();
 
-        setMovimientos(0, listaMovimientos.buscarMovimientoPorNombre("Moflete Est├ítico"));
+        setMovimientos(0, listaMovimientos.buscarMovimientoPorNombre("Moflete Estático"));
         setMovimientos(1, listaMovimientos.buscarMovimientoPorNombre("Impactrueno"));
         setMovimientos(2, listaMovimientos.buscarMovimientoPorNombre("Chispa"));
         setMovimientos(3, listaMovimientos.buscarMovimientoPorNombre("Chispazo"));
     }
     
-     @Override
+    @Override
     public double obtenerEfectividad(Pokemon pokemonAtacante, int m){
         double efectividad = 1.0;
         if(pokemonAtacante.getMovimientos(m).getTipo()== Tipo.ACERO) efectividad = 0.5;
@@ -27,6 +27,22 @@ public class Pikachu extends Pokemon {
         if(pokemonAtacante.getMovimientos(m).getTipo()== Tipo.VOLADOR) efectividad = 2.0;
 
         return efectividad;
+    }
+
+    @Override
+    public double calcularBonificacion(Pokemon pokemon, int pos) {
+        double bonificacion = 1.0; // Valor predeterminado
+        Movimiento movimiento = this.getMovimientos(pos);
+
+        // Lógica para calcular la bonificación según las reglas dadas
+        Tipo tipoMovimiento = movimiento.getTipo();
+        if (this.getTipo() == tipoMovimiento) {
+            bonificacion = 1.5;
+        } else {
+            bonificacion = 1.0;
+        }
+
+        return bonificacion;
     }
 
     public Tipo getTipo() {
